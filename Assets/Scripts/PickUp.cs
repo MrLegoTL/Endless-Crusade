e607366fe1a91ba344af52496cdadd7a9e2794a2
MrLegoTL <martingarciacurto@gmail.com>
2023-12-17 20:48:41 +0100

    Cambios

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //cremaos un tipo de enumerado para especificar el tipo de coleccionable
-public enum CollectableType { collectable, powerup }
+public enum CollectableType { collectable, powerup, HealthGem}
 
 //este script requerira que exita un componente de tipo AudioSource
 [RequireComponent(typeof(AudioSource))]
@@ -12,12 +12,17 @@ public class PickUp : MonoBehaviour
 {
     //especificamos el tipo de coleccionable, inicializandolo con el valor collectable
     public CollectableType type = CollectableType.collectable;
+    [Header("PowerUp")]
     public float timeMoreDamage;
+    [Header("Health Gem")]
+    public float healthToRestore;
+    [Header("SoulGem")]
     public int soulValue = 1;
     public AudioClip audioClip;
     public AudioSource audioSource;
     public Collider2D collider;
     public SpriteRenderer spriteRenderer;
+     PlayerController player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -28,12 +33,19 @@ public class PickUp : MonoBehaviour
                  GameManager.instance.PickupCollectable(soulValue);
                     break;
                 case CollectableType.powerup:
-                    PlayerController player = collision.GetComponent<PlayerController>();
+                     player = collision.GetComponent<PlayerController>();
                     if (player != null) player.ActivatePowerUp(timeMoreDamage);
+                    break;
+                case CollectableType.HealthGem:
+                    player = collision.GetComponent<PlayerController>();
+                    if(player!=null)player.RestoredHealth(healthToRestore);
                     break;
                 default:
                     break;
             }
+
+           
+            
             EffectSound();
             DeactivateCollactable();
         }
